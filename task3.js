6a3a0121f4e0583f634c7d83938d44a7ced078b0
Mark Vaisman <mark.vaisman@mail.ru>
2018-05-29 23:47:37 +1100

    Task3 v.1.0.0

---
+++ task3.js
@@ -0,0 +1,12 @@
+var a = parseInt(process.argv[2]),
+    b = parseInt(process.argv[3]);
+
+while(a != 0 && b != 0) {
+    if ( a > b ) {
+        a %= b;
+    } else {
+        b %= a;
+    }
+}
+
+process.stdout.write((a + b).toString());
