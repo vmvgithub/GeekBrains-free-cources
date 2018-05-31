6ab7af832b759242ca8098a301a4d2797c706198
Mark Vaisman <mark.vaisman@mail.ru>
2018-05-31 23:20:19 +1100

    Task2 v.1.0.0

--- task2.js
+++ task2.js
@@ -1,3 +1,19 @@
 /* Task 2 */
 
-/* Put your code here */
+var str = process.argv[2],
+    lastIndex = 0,
+    maxPrefix = '',
+    prefix,
+    template,
+    result;
+
+for(var k = 1; k < str.length; k++) {
+    prefix = str.substr(0, k);
+    template = new RegExp('\\B' + prefix);
+    result = str.match(template);
+    if (result && result.index >= lastIndex ) {
+	maxPrefix = prefix;
+    }
+}
+
+process.stdout.write(maxPrefix);
